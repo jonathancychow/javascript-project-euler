4f1d140fa99df279679a17a34356bbb6a66ab406
Jonathan Chow <jonathan.chow@mclaren.com>
2021-02-05 16:00:05 +0000

    WIP

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 // import {clear} from './function.js';
 
 function myFunction() {
-  console.log('dfsdfsdf')
+  // console.log(typeof(3)==='number')
   var x = document.getElementById("Input").value;
+  // console.log(typeof(x))
+  console.log(isPrimeNumber(Number(x)))
   document.getElementById("result").innerHTML = "You wrote: " + x;
+}
+
+function isPrimeNumber(N){
+  if (typeof(N)==='number'){
+    // console.log('print')
+    if (N===2 || N===3){
+      return true;
+    }else if(N % 2 === 0 || N < 2){
+      return false;
+    }
+    upperLimit = Math.floor(N**0.5)+1;
+    for(i=3; i < upperLimit;  i++){
+      if (N % i === 0 ){
+        return false;
+      }
+    }
+    return true;
+  }else{
+    return false;
+  }
+  
 }
