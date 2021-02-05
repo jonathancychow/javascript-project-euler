fe7a45543eeb18d7577168fc1dc2e3b179190b9d
Jonathan Chow <jonathan.chow@mclaren.com>
2021-02-05 12:29:35 +0000

    Set up backbone

---
+++ app.js
@@ -0,0 +1,7 @@
+// import {clear} from './function.js';
+
+function myFunction() {
+  console.log('dfsdfsdf')
+  var x = document.getElementById("Input").value;
+  document.getElementById("result").innerHTML = "You wrote: " + x;
+}
